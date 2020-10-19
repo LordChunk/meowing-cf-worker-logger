286bff82463d22e0c1bde5bf2434ea8c540b35ca
Job <contact@chu.mk>
2020-10-19 19:05:24 +0200

    Added basic country request listing

--- website/src/app/dashboard/dashboard.component.ts
+++ website/src/app/dashboard/dashboard.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
+import { map, mapTo } from 'rxjs/operators';
 import { CfRequestLog } from '../models/cf-request-log.model';
 
 @Component({
@@ -9,9 +10,27 @@ import { CfRequestLog } from '../models/cf-request-log.model';
   styleUrls: ['./dashboard.component.scss'],
 })
 export class DashboardComponent {
-  logs: Observable<CfRequestLog[]>;
+  public logs: Observable<CfRequestLog[]>;
+  public countryCount: Observable<Array<{ country: string, count: number }>>;
 
   constructor(firestore: AngularFirestore) {
     this.logs = firestore.collection<CfRequestLog>('logs').valueChanges();
+
+    this.countryCount = this.logs.pipe(
+      map(logArray => {
+        const countries = logArray.map(log => log.request.cf.country);
+        const countryNameCount: Array<{ country: string, count: number }> = [];
+
+        countries.forEach(countryName => {
+          const country = countryNameCount.find(item => item.country === countryName);
+          if (country !== undefined) {
+            country.count++;
+          } else {
+            countryNameCount.push({ country: countryName, count: 1 });
+          }
+        });
+
+        return countryNameCount.sort((a, b) => b.count - a.count);
+      }));
   }
 }
