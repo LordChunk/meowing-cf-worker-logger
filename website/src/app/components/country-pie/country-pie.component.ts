d6fcee7728136539299c65d70ffb3e0c9449f3e8
Job <contact@chu.mk>
2020-10-19 21:03:22 +0200

    Added some colours for the pie chart

--- website/src/app/components/country-pie/country-pie.component.ts
+++ website/src/app/components/country-pie/country-pie.component.ts
@@ -20,7 +20,10 @@ export class CountryPieComponent implements OnInit {
   public pieChartType: ChartType = 'pie';
   public pieChartLegend = true;
   public pieChartPlugins = [];
-  public pieChartColors = [];
+  public pieChartColors = [
+    {
+      backgroundColor: ['#1f77b4', '#aec7e8', '#ff7f0e', '#ffbb78', '#2ca02c', '#98df8a', '#d62728', '#ff9896', '#9467bd', '#c5b0d5', '#8c564b', '#c49c94', '#e377c2', '#f7b6d2', '#7f7f7f', '#c7c7c7', '#bcbd22', '#dbdb8d', '#17becf', '#9edae5']
+    }];
 
   ngOnInit(): void {
     this.countries.subscribe(countries => {
