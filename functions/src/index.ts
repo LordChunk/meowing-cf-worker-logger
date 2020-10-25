6bd213c0dc0dfd5f8fe3db20604514944f77ad43
Job <contact@chu.mk>
2020-10-26 00:52:00 +0100

    Converting request headers to an object array is now handled on the CF edge

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -10,19 +10,10 @@ app.get('/', (req, res) => res.send('Hello world'));
 app.post('/logrequest', async (request, response) => {
   try {
       const data = JSON.parse(JSON.stringify(request.body));
-      const headers: Array<Array<string>> =  data.headers;
-
-      const headerObject: any = {};
-      headers.forEach(headerArray => {
-        headerObject[headerArray[0]] = headerArray[1];
-      });
-
-      const dataWithHeaders = data;
-      dataWithHeaders.headers = headerObject;
 
       const dbObject = {
         date: new Date().toISOString(),
-        request: dataWithHeaders
+        request: data
       };
 
       await admin.firestore().collection('logs').add(dbObject);
