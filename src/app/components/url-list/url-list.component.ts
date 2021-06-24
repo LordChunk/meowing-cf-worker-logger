240c032da8952075b3fd0e564c6d3dba402cae03
Job <LordChunk@users.noreply.github.com>
2021-06-24 13:36:15 +0200

    Added collumn sorting

--- src/app/components/url-list/url-list.component.ts
+++ src/app/components/url-list/url-list.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input, ViewChild } from '@angular/core';
-import { MatSort } from '@angular/material/sort';
-import { Observable, of } from 'rxjs';
+import { MatSort, Sort } from '@angular/material/sort';
+import { BehaviorSubject } from 'rxjs';
 import { TreemapChartInput } from '../treemap-chart/treemap-chart.component';
 
 @Component({
@@ -12,7 +12,32 @@ export class UrlListComponent {
 
   @ViewChild(MatSort) sort!: MatSort;
 
-  @Input() ListData: Observable<TreemapChartInput[]> = of([]);
+  @Input() ListData: BehaviorSubject<TreemapChartInput[]> = new BehaviorSubject<TreemapChartInput[]>([]);
+
 
   displayedColumns: string[] = ['shortLabel', 'value', 'label'];
+
+  sortData(sort: Sort) {
+    const data = this.ListData.getValue().slice();
+    if (!sort.active || sort.direction === '') {
+      this.ListData.next(data);
+      return;
+    }
+
+    const sortedData = data.sort((a, b) => {
+      const isAsc = sort.direction === 'asc';
+      switch (sort.active) {
+        case 'label': return compare(a.label, b.label, isAsc);
+        case 'shortLabel': return compare(a.shortLabel, b.shortLabel, isAsc);
+        case 'value': return compare(a.value, b.value, isAsc);
+        default: return 0;
+      }
+    });
+
+    this.ListData.next(sortedData);
+  }
+}
+
+function compare(a: number | string, b: number | string, isAsc: boolean) {
+  return (a < b ? -1 : 1) * (isAsc ? 1 : -1);
 }
