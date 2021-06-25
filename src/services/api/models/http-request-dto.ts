04cecd62bd5e4420bfec41a660f1f5b1380bebbf
Job <LordChunk@users.noreply.github.com>
2021-06-25 22:26:17 +0200

    Updated models

--- src/services/api/models/http-request-dto.ts
+++ src/services/api/models/http-request-dto.ts
@@ -1,10 +1,8 @@
 /* tslint:disable */
 /* eslint-disable */
-import { CfHttpHeader } from './cf-http-header';
 export interface HttpRequestDto {
   body?: null | string;
   bodyUsed?: boolean;
-  cf?: CfHttpHeader;
   contentLength: number;
   fetchers?: null | string;
   headers?: null | Array<Array<string>>;
--- src/services/api/models/http-request.ts
+++ src/services/api/models/http-request.ts
@@ -1,12 +1,10 @@
 /* tslint:disable */
 /* eslint-disable */
-import { CfHttpHeader } from './cf-http-header';
 import { HttpHeader } from './http-header';
 import { RequestUrl } from './request-url';
 export interface HttpRequest {
   body?: null | string;
   bodyUsed?: boolean;
-  cf?: CfHttpHeader;
   contentLength: number;
   fetchers?: null | string;
   headers?: null | Array<HttpHeader>;
